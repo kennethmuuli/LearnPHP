513c926605f147d6727bf83b8c9f715808c14403
Kenneth Muuli <kenneth.muuli@gmail.com>
2023-04-28 14:05:29 +0300

    Added models

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -2,29 +2,19 @@
 
 namespace App\Controllers;
 
+use App\DB;
+use App\Models\Post;
 use PDO;
 use PDOException;
 
 class PublicController {
     public function index() {
-        try {
-            $conn = new PDO("sqlite:database.sqlite");
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $conn->prepare("SELECT * FROM posts;");
-            $stmt->execute();
-
-            // set the resulting array to associative
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            $result = $stmt->fetchAll();
-            dump($result);
-          } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-          } die;
-        
-        $name = 'Kenneth';
-        view('index', compact('name'));
+      
+      $result = Post::all();
+      dd($result);
+      
+      $name = 'Kenneth';
+      view('index', compact('name'));
     }
 
     public function about() {
---
+++ src/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\DB;
+
+class User extends Model {
+    static $table = 'users';
+    public $id;
+    public $email;
+    public $password;
+
+}
